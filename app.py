7a5e3edb48f020086cf684de86641625cf317cf7
clairtonslv45-ai <clairtonslv45@gmail.com>
2026-01-25 19:39:11 -0300

    Fix: Procfile and port configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -48,4 +49,5 @@ def player():
     return render_template('filme.html', filme={"titulo": request.args.get('t'), "link": request.args.get('l')})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
