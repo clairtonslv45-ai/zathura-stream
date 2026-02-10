2b17157315e65787289d814d560288ff97951170
clairtonslv45-ai <clairtonslv45@gmail.com>
2026-02-10 12:24:53 -0300

    Adicionado CJ7 o brinquedo mágico

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ filmes_db = {
         {"titulo": "Olhos famintos 1 (2001) Dublado", "link": "https://ok.ru/videoembed/383529192100"}
     ],
     "comedia": [
+        {"titulo": "CJ7 o brinquedo mágico", "link": "https://ok.ru/videoembed/10147592276650"},
         {"titulo": "Kung Pow: O Mestre da Confusão", "link": "https://ok.ru/videoembed/9743619852946"},
         {"titulo": "Pelas garotas e pela glória (2009)", "link": "https://ok.ru/videoembed/6681522866916"},
         {"titulo": "Frat Party (A festa) (2009)", "link": "https://ok.ru/videoembed/6695168969223"},
