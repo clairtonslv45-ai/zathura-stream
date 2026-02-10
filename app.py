a7a0761259b56e7021ea5b77b596b032e3389a24
clairtonslv45-ai <clairtonslv45@gmail.com>
2026-02-10 12:09:58 -0300

    Adicionado Reino Escondido

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ filmes_db = {
         {"titulo": "A Lenda dos Guardiões", "link": "https://ok.ru/videoembed/383529192100"},
         {"titulo": "Planeta do Tesouro", "link": "https://ok.ru/videoembed/1551464860379"},
         {"titulo": "Planeta 51", "link": "https://ok.ru/videoembed/7251048598034"},
-        {"titulo": "A Fuga do Planeta Terra", "link": "https://ok.ru/videoembed/4899872443050"}
+        {"titulo": "A Fuga do Planeta Terra", "link": "https://ok.ru/videoembed/4899872443050"},
+        {"titulo": "Reino Escondido", "link": "https://ok.ru/videoembed/1385257044634"}
     ],
     "cinema-antigo": [
         {"titulo": "Clube dos Cinco (1985)", "link": "https://ok.ru/videoembed/8473915624001"},
